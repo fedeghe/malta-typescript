ff3b53ca9d1ff1bf6a3174e11f27abefa7751e51
federicoghedinastailamedia <federico.ghedina@stailamedia.com>
2016-11-11 13:46:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('malta').checkExec('tsc');
+// this is somehow a special case cause jsdoc is meant to be used only as console tool
+// then:
+// - we cannot test with require
+// - we do not need to require, it's (hopefully) installed
+// - we need to lauch it as a new process
+// 
+// var deps = ['jsdoc'];
+// deps && deps.length && require('malta2').checkDeps(deps);
+
+
+// 
+// http://usejsdoc.org/
+
+var path = require('path'),
+	fs = require('fs'),
+	child_process = require('child_process');
+
+function malta_typescript(o, options) {
+
+	var self = this,
+		start = new Date(),
+		msg,
+		oldname = o.name;
+
+	return function (solve, reject){
+		var ls = child_process.spawn('tsc', [o.name]);
+		
+		ls.on('exit', function (code) {
+			o.name = o.name.replace(/\.ts$/, '.js');
+			o.content = fs.readFileSync(o.name) + "";
+			msg = 'plugin ' + path.basename(__filename) + ' wrote ' + o.name;
+			fs.unlink(oldname);
+			solve(o);
+			self.notifyAndUnlock(start, msg);
+		});
+
+		ls.stdout.on('data', function(data) {
+			self.log_debug(data + "");
+		});
+
+		ls.stderr.on('error', function (data) {
+			self.log_err('stderr: ' + data);
+		});
+	};
+}
+malta_typescript.ext = 'ts';
+module.exports = malta_typescript;
